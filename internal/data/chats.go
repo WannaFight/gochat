19468410fe3fd08c2fdf8aa28333209fe27fe183
Ivan Trushin <itrushin99@gmail.com>
2024-06-13 21:22:40 +0300

    feat: add users, chats, chat_members db wrapper

--- internal/data/chats.go
+++ internal/data/chats.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -28,8 +30,61 @@ type ChatModel struct {
 	DB *sql.DB
 }
 
+func (m ChatModel) GetByUUID(uuid uuid.UUID) (*Chat, error) {
+	chat := new(Chat)
+	query := `
+		SELECT uuid, created_at
+		FROM chats
+		WHERE uuid = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, uuid).Scan(&chat.UUID, &chat.CreatedAt)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, errors.New("not found")
+		default:
+			return nil, err
+		}
+	}
+	return chat, nil
+}
+
 type ChatMemberModel struct {
 	DB *sql.DB
+}
+
+func (m ChatMemberModel) GetChatMembersByChat(chatUUID uuid.UUID) ([]*ChatMember, error) {
+	query := `
+		SELECT user_id, is_owner
+		FROM chat_members
+		WHERE chat_members.chat_uuid = $1`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, query, chatUUID)
+	if err != nil {
+		return nil, err
+	}
+
+	chatMembers := []*ChatMember{}
+	for rows.Next() {
+		var member ChatMember
+		err := rows.Scan(&member.UserID, &member.IsOwner)
+		if err != nil {
+			return nil, err
+		}
+
+		chatMembers = append(chatMembers, &member)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return chatMembers, nil
 }
 
 type ChatMessageModel struct {
